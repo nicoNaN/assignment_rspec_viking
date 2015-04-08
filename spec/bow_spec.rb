bb88676eb89b1c090c79cf825b09852c2c08e751
niconan <nick.hax@gmail.com>
2015-04-08 10:22:04 -0700

    set up environment & finish bow spec

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,25 @@
-# Your code here
+require 'weapons/bow'
+
+describe Bow do
+  let(:bow) { Bow.new }
+  describe ':arrows' do
+    it 'is accessible' do
+      expect(bow).to respond_to(:arrows)
+    end
+    it 'starts with 10 arrows by default' do
+      expect(bow.arrows).to eq(10)
+    end
+    let(:biggest_quiver) { Bow.new(50) }
+    it 'starts with a specified number of arrows when passed as an arg' do
+      expect(biggest_quiver.arrows).to eq(50)
+    end
+    it 'is reduced by 1 when used' do
+      bow.use
+      expect(bow.arrows).to eq(9)
+    end
+    it 'throws an error when used at zero' do
+      10.times { bow.use }
+      expect { bow.use }.to raise_error
+    end
+  end
+end
